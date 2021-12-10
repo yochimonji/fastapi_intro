937820fdb91faa3da887ec5020c3269a29ff3fb5
yochimonji <yochimonji@gmail.com>
2021-12-11 02:29:56 +0900

    add レスポンスのスキーマ定義

--- api/routers/task.py
+++ api/routers/task.py
@@ -1,10 +1,14 @@
+from typing import List
+
 from fastapi import APIRouter
+
+import api.schemas.task as task_schema
 
 router = APIRouter()
 
-@router.get("/tasks")
-async def list_task():
-    pass
+@router.get("/tasks", response_model=List[task_schema.Task])
+async def list_tasks():
+    return [task_schema.Task(id=1, title="1つ目のTODOタスク")]
 
 @router.post("/tasks")
 async def create_task():
